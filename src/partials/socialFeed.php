3669fd93df7c243c9c05a7000a7a3a7467629b37
Philipp Timmalog <ptimmalog@gmail.com>
2016-06-18 11:06:59 +0200

    feat: Text in impressions being retrieved from backend

--- src/partials/socialFeed.php
+++ src/partials/socialFeed.php
@@ -1,12 +1,21 @@
 
-<h2>Impressionen </h2>
-
 <?php
 $query = new WP_Query(array('post_type' => 'socialfeed'));
 if($query -> have_posts()) {
   while ( $query->have_posts() ) {
     $query->the_post();
-    the_content();
+
+    echo '<h2>';
+    echo the_title();
+    echo '</h2>';
+
+    echo '<p class="col">';
+    echo strip_tags( nl2br( get_the_content( '' ) ), "" );
+    echo '</p>';
+
+    echo '<div class="feedContainer">';
+    echo do_shortcode(get_field('socialfeed'));
+    echo '</div>';
   }
   wp_reset_query();
 }
