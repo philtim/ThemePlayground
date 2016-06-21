4cc6bfd2e4284c0f313ede794da6c2ddd6ce0612
Philipp Timmalog <ptimmalog@gmail.com>
2016-06-21 02:50:54 +0200

    feat: Footer

--- src/footer.php
+++ src/footer.php
@@ -14,10 +14,44 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'fischertruck' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'fischertruck' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'fischertruck' ), 'fischertruck', '<a href="http://www.t7lab.com" rel="designer">fischerwerke GmbH & Co. KG</a>' ); ?>
+		<div class="site-info / container">
+      <div class="row">
+        <div class="logo / col-xs-12 col-sm-3">
+          <div class="logo">Logo</div>
+        </div>
+        <div class="col-xs-12 col-sm-9">
+
+          <div class="row">
+            <div class="socialMedia / col-xs-123 col-sm-33">
+              <ul>
+                <li><span><i class="fa fa-lg fa-facebook" aria-hidden="true"></i> Facebook</span></li>
+                <li><span><i class="fa fa-lg fa-instagram" aria-hidden="true"></i> Instagram</span></li>
+                <li><span><i class="fa fa-lg fa-youtube" aria-hidden="true"></i> Youtube</span></li>
+              </ul>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="links / col-xs-132 col-sm-33">
+              <ul>
+                <li><span>Datenschutz</span></li>
+                <li><span>Impressum</span></li>
+                <li><span>Allgemeine Geschäftsbedingungen</span></li>
+              </ul>
+            </div>
+          </div>
+
+        </div>
+
+
+
+
+
+
+      </div>
+
+
+
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
