bcbcc5a930c93df835c8b2f1bd955efdffc91684
Philipp Timmalog <ptimmalog@gmail.com>
2016-06-23 22:58:38 +0200

    refactor: Comine tourtruck inside with backend

--- src/partials/truckInformation.php
+++ src/partials/truckInformation.php
@@ -1,40 +1,27 @@
-<!--            --><?php
-//            $args = array(
-//              'post_type' => 'truckinformation',
-//              'order' => 'ASC',
-//            );
-//            $query = new WP_Query($args);
-//            if($query -> have_posts()) {
-//              while ( $query->have_posts() ) {
-//                $query->the_post();
-//                the_content();
-//              }
-//              wp_reset_query();
-//            }
+
+<?php
+$query = new WP_Query(array('post_type' => 'truckinformation'));
+if($query -> have_posts()) {
+  while ( $query->have_posts() ) {
+    $query->the_post();
+
+    echo '<h2>';
+    echo the_title();
+    echo '</h2>';
+
+    echo '<p class="col">';
+    echo strip_tags( nl2br( get_the_content( '' ) ), "" );
+    echo '</p>';
+
+    if (get_field( 'video' )) {
+      echo '<div class="videoWrapper">';
+      echo '<iframe class="center-block" width="560" height="314" src="';
+      echo get_field( 'video' );
+      echo '" frameborder="0" allowfullscreen></iframe>';
+      echo '</div>';
+    }
+  }
+  wp_reset_query();
+}
 ?>
-<h2>TourTruck inside</h2>
 
-<p>
-  Der fischer TourTruck von innen: mobile Akademie und Erlebnis-Location.
-</p>
-
-
-<!--    <div id="x" class="player"-->
-<!--         data-property="{-->
-<!--         videoURL:'http://youtu.be/KKQfim1IJLM',-->
-<!--         containment:'self',-->
-<!--         addRaster:true,-->
-<!--         autoPlay:true,-->
-<!--         showControls:false,-->
-<!--         mute:true,-->
-<!--         startAt:0,-->
-<!--         opacity:1}">-->
-<!--    </div>-->
-
-<!--<div class="row">-->
-<!--  <div class="col-xs-12">-->
-<div class="videoWrapper">
-    <iframe class="center-block" width="560" height="314" src="https://www.youtube.com/embed/o8lBs_AZ1-g?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
-</div>
-<!--  </div>-->
-<!--</div>-->
